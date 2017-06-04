fbd9dc20e81374dc4cca88e07377fe06dedd6aed
babuwpd <babuwpd@gmail.com>
2017-06-04 22:29:26 +0600

    class-50

---
+++ class-50/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        $(".listing-carousel").owlCarousel({
+            items:3,
+            nav: false,
+            dots: true,
+            margin:30,
+            autoplay: true,
+            loop: true,
+        });
+    });
+
+
+    jQuery(window).load(function(){
+        
+    });
+
+
+}(jQuery));	
