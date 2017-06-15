d15215ee9700c89502bced8b7a443212070f2728
babuwpd <babuwpd@gmail.com>
2017-06-16 05:04:29 +0600

    babu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,14 @@
     jQuery(document).ready(function($){
 
 
-        $(".video-play-btn").magnificPopup({
-            type:'video',
+        $(".homepage-slides").owlCarousel({
+            items:1,
+            nav:true,
+            dots: true,
+            autoplay: false,
+            navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+            loop:true,
+            animateOut: 'fadeOut',
         });
         
 
